a4f9db145b2b02677084788b18d655ae7566c8d1
ChaseParate <97500441+ChaseParate@users.noreply.github.com>
2023-01-07 15:37:44 -0700

    Update example

--- examples/get_all_type_names.rs
+++ examples/get_all_type_names.rs
@@ -2,27 +2,10 @@
 async fn main() {
     // Gets the names of all pokmon types. Retrieving them 5 by 5 from the api.
     let rustemon_client = rustemon::client::RustemonClient::default();
-    let mut offset = 0;
-    let limit = 5;
-    let number_of_type = rustemon::pokemon::type_::get_page(&rustemon_client)
+    let types = rustemon::pokemon::type_::get_all_pages(&rustemon_client)
         .await
-        .unwrap()
-        .count;
-    let mut type_names: Vec<String> = vec![];
-    while offset < number_of_type {
-        match rustemon::pokemon::type_::get_page_with_param(offset, limit, &rustemon_client).await {
-            Ok(page) => {
-                for name in page.results {
-                    type_names.push(name.name);
-                }
-                offset += limit;
-            }
-            Err(err) => {
-                println!("Error occured : {}", err);
-                break;
-            }
-        }
-    }
+        .unwrap();
+    let type_names: Vec<String> = types.into_iter().map(|type_| type_.name).collect();
 
     println!("All Pokémon type names : {:#?}", type_names);
 }
