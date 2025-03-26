7e572ed74e19ad4f52e68c438f0bb87114ac7ef2
Martin Lemesle <lemesle.martin@hotmail.fr>
2025-03-26 07:52:08 +0100

    MR review

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -1,17 +1,21 @@
 macro_rules! endpoint {
     (unnamed $type:ty; for $name:literal) => {
-        use crate::model::resource::{UnnamedApiResourceList, UnnamedApiResource};
-        type ActualApiResourceList<T> = UnnamedApiResourceList<T>;
-        type ActualApiResource<T> = UnnamedApiResource<T>;
-        crate::endpoint!(inner $type; for $name);
+        use crate::model::resource::{ApiResourceList, ApiResource};
+
+        type ResourceList<T> = ApiResourceList<T>;
+        type Resource<T> = ApiResource<T>;
+
+        crate::endpoint!(@inner $type; for $name);
     };
     ($type:ty; for $name:literal) => {
         use crate::model::resource::{NamedApiResourceList, NamedApiResource};
-        type ActualApiResourceList<T> = NamedApiResourceList<T>;
-        type ActualApiResource<T> = NamedApiResource<T>;
-        crate::endpoint!(inner $type; for $name);
+
+        type ResourceList<T> = NamedApiResourceList<T>;
+        type Resource<T> = NamedApiResource<T>;
+
+        crate::endpoint!(@inner $type; for $name);
     };
-    (inner $type:ty; for $name:literal) => {
+    (@inner $type:ty; for $name:literal) => {
         use crate::client::{RustemonClient, Id};
         use crate::error::Error;
 
@@ -20,8 +24,8 @@ macro_rules! endpoint {
         /// # Arguments
         ///
         /// `rustemon_client` - The [RustemonClient] to use to access the resource.
-        pub async fn get_page(rustemon_client: &RustemonClient) -> Result<ActualApiResourceList<$type>, Error> {
-            rustemon_client.get_by_endpoint::<ActualApiResourceList<$type>>($name).await
+        pub async fn get_page(rustemon_client: &RustemonClient) -> Result<ResourceList<$type>, Error> {
+            rustemon_client.get_by_endpoint::<ResourceList<$type>>($name).await
         }
 
         /// Returns the page targeted by the parameters.
@@ -35,8 +39,8 @@ macro_rules! endpoint {
             offset: i64,
             limit: i64,
             rustemon_client: &RustemonClient
-        ) -> Result<ActualApiResourceList<$type>, Error> {
-            rustemon_client.get_with_limit_and_offset::<ActualApiResourceList<$type>>($name, limit, offset).await
+        ) -> Result<ResourceList<$type>, Error> {
+            rustemon_client.get_with_limit_and_offset::<ResourceList<$type>>($name, limit, offset).await
         }
 
         /// Returns all entries from the given resource.
@@ -44,7 +48,7 @@ macro_rules! endpoint {
         /// # Arguments
         ///
         /// `rustemon_client` - The [RustemonClient] to use to access the resource.
-        pub async fn get_all_entries(rustemon_client: &RustemonClient) -> Result<Vec<ActualApiResource<$type>>, Error> {
+        pub async fn get_all_entries(rustemon_client: &RustemonClient) -> Result<Vec<Resource<$type>>, Error> {
             let mut first_page = get_page(rustemon_client).await?;
             let first_page_entries_count = first_page.results.len() as i64;
 
